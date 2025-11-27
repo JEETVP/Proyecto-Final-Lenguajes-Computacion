e24de8241b42f05402a5708894a6c24c1c6fe0a3
JEETVP <roberto.villegasrvo@gmail.com>
2025-11-26 20:54:34 -0600

    correcciones en cifrado simetrico 4

--- src/routes/cryptoRoutes.js
+++ src/routes/cryptoRoutes.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { sha256Hash, argon2Hash, aesEncrypt, aesDecrypt, chacha20Encrypt, chacha20Decrypt, rsaEncrypt, rsaDecrypt, signDsa, verifyDsa } = require('../controllers/cryptoController');
+const { 
+  sha256Hash, 
+  argon2Hash, 
+  aesEncrypt, 
+  aesDecrypt, 
+  chacha20Encrypt, 
+  chacha20Decrypt, 
+  generateRsaKey,  
+  generateDsaKey 
+} = require('../controllers/cryptoController');
 
 // Endpoints de Criptografía
 
@@ -9,6 +18,7 @@ router.post('/hash/sha256', sha256Hash);  // Generar hash con SHA-256
 
 // Rutas de Argon2
 router.post('/hash/argon2', argon2Hash);  // Generar hash con Argon2
+
 // Rutas para AES-256-CBC
 router.post('/encrypt/aes_cbc', aesEncrypt);  // Cifrar con AES
 router.post('/decrypt/aes_cbc', aesDecrypt);  // Descifrar con AES
@@ -21,9 +31,12 @@ router.post('/decrypt/chacha20', chacha20Decrypt);  // Descifrar con ChaCha20
 router.post('/generate/rsa', generateRsaKey);  // Generar clave RSA
 router.post('/generate/dsa', generateDsaKey);  // Generar clave DSA
 
+module.exports = router;
+
+
 /*router.post('/encrypt/rsa', rsaEncrypt);
 router.post('/decrypt/rsa', rsaDecrypt);
 router.post('/sign/dsa', signDsa);
 router.post('/verify/dsa', verifyDsa);*/
 
-module.exports = router;
+
